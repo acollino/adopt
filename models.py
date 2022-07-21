970bad3692ad84d0839854710e63cf78c8d719dd
Alex Collino <arcollino@gmail.com>
2022-07-21 12:58:20 -0700

    implement pet model

---
+++ models.py
@@ -0,0 +1,19 @@
+from email.policy import default
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    """Connect to the Adoption database"""
+    db.app = app
+    db.init_app(app)
+
+class Pet(db.Model):
+    """Class representing a pet from the Adoption Agency"""
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, nullable=False)
+    species = db.Column(db.Text, nullable=False)
+    photo_url: db.Column(db.Text)
+    age = db.Column(db.Integer)
+    notes: db.Column(db.Text)
+    available: db.Column(db.Boolean, nullable=False, default=True)
